5746f533518bc81b986d4adaa5d4ae09f537314e
anastasiarn1 <anastasia.r.n1@gmail.com>
2024-04-19 12:28:19 -0700

    python file
    just comments for potential logic and pandas import

---
+++ hackathon.py
@@ -0,0 +1,42 @@
+import pandas as pd
+#import database
+
+# Student will input or import their schedule
+# Student will select event topics they’re interested in
+# Schedule will print all events relevant to interests selected by student and fits into schedule
+
+
+#input:
+    #tags
+    #class schedule for the week
+    #any specific blocked out times
+
+
+#int tag = 0
+#int tag = 0 
+#int tag = 0
+#int tag = 0
+#front end will switch these on when ticked on the website?? 
+
+#-----------------------------------------------------------------
+#   CLASS SCHEDULE
+
+#for day of week
+    #if no class: pass
+    #else: for class in that day
+            #no class: pass, else
+                #apply to calendar in time slot
+                #tag time as "not free"
+
+#-----------------------------------------------------------------
+#    EVENT SCHEDULE
+
+#for day of week
+    #if no events: pass     
+    #else: for events tagged with that day  (4/21, 4/22, 4/23, etc)
+            #if tag == 1 && time is "free"
+                #apply to calendar
+
+#-----------------------------------------------------------------
+
+#export to csv 
